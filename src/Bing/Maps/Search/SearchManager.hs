c27f6d5622c2e17a45c6bfc94efbe1ed4efff5da
Eric Wong <ericsyw@gmail.com>
2016-09-19 17:13:17 +0800

    Add JS function to do geocoding directly and the new API is in Async mode

--- src/Bing/Maps/Search/SearchManager.hs
+++ src/Bing/Maps/Search/SearchManager.hs
@@ -3,7 +3,7 @@ module Bing.Maps.Search.SearchManager where
 
 import GHCJS.Types
 import GHCJS.Marshal
-
+import Data.JSString
 import JavaScript.Object
 
 import Bing.Maps.Map
@@ -19,3 +19,22 @@ foreign import javascript unsafe "($2)['geocode']($1)"
 
 geocode :: GeocodeRequestOptions -> SearchManager -> IO ()
 geocode opt mngr = toJSGeoReqOption opt >>= (flip js_geocode mngr . jsval)
+
+foreign import javascript interruptible "Microsoft['Maps']['loadModule']('Microsoft.Maps.Search', { callback: function() {\
+                                        \    var sm = new Microsoft['Maps']['Search']['SearchManager']($2);\
+                                        \    sm.geocode({where: ($1), \
+                                        \                count: 10,\
+                                        \                callback: function(res) { \
+                                        \                              var plcRes = res['results'][0];\
+                                        \                              var l = plcRes['location'];\
+                                        \                              var lat = l['latitude'];\
+                                        \                              var lng = l['longitude'];\
+                                        \                              $c(lat, lng);\
+                                        \                          },\
+                                        \                errorCallback: function () { return; }\
+                                        \    });\
+                                        \}});"
+    js_geocodeAsync :: JSString -> Map -> IO (Double, Double)
+
+geocodeAsync :: String -> Map -> IO (Double, Double)
+geocodeAsync addr map = js_geocodeAsync (pack addr) map
